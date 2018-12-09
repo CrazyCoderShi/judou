2f355ce46a684d05c9fafa0b4df0218dfcda346e
天南-mini <363605775@qq.com>
2018-12-09 22:45:09 +0800

    feat: layouts

--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -24,20 +24,96 @@ class _ProfilePageState extends State<ProfilePage> {
     });
   }
 
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(title: Text('Shopping List')),
+//      body: ListView(
+//        padding: EdgeInsets.symmetric(vertical: 8.0),
+//        children: widget.products.map((Product product) {
+//          return ShoppingListItem(
+//            product: product,
+//            inCart: _shoppingCart.contains(product),
+//            onCartChanged: _handleCartChanged
+//          );
+//        }).toList()
+//      ),
+//    );
+//  }
+
+  // 顶部文字
+  Widget titleSection() => Container(
+    padding: const EdgeInsets.all(32.0),
+    child: Row(
+      children: <Widget>[
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(fontWeight: FontWeight.bold)
+                ),
+              ),
+              Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(color: Colors.grey[500])
+              )
+            ],
+          ),
+        ),
+        Icon(Icons.star, color: Colors.red[500]),
+        Text('41')
+      ],
+    ),
+  );
+
+  // 构建icon + text的button
+  Column buildButtonColumn(IconData icon, String label) {
+    Color color = Theme.of(context).primaryColor;
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8.0),
+          child: Text(
+            label,
+            style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w400, color: color)
+          )
+        )
+      ],
+    );
+  }
+
+  Widget buttonSection() => Container(
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        buildButtonColumn(Icons.call, 'CALL'),
+        buildButtonColumn(Icons.near_me, 'ROUTE'),
+        buildButtonColumn(Icons.share, 'SHARE')
+      ],
+    )
+  );
+
+  // 文字描述部分
+  Widget textSection() => Container(
+    padding: const EdgeInsets.all(32.0),
+    child: Text(
+      '''
+      Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.
+      ''',
+      softWrap: true
+    )
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Shopping List')),
-      body: ListView(
-        padding: EdgeInsets.symmetric(vertical: 8.0),
-        children: widget.products.map((Product product) {
-          return ShoppingListItem(
-            product: product,
-            inCart: _shoppingCart.contains(product),
-            onCartChanged: _handleCartChanged
-          );
-        }).toList()
-      ),
-    );
+    return titleSection();
   }
 }
