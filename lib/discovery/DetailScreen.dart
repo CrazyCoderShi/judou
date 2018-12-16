befea6c2b8b89f5965246ab901521433f1b12f11
天南-mini <363605775@qq.com>
2018-12-16 22:04:08 +0800

    feat: tabs

--- lib/discovery/DetailScreen.dart
+++ lib/discovery/DetailScreen.dart
@@ -1,19 +1,56 @@
 import 'package:flutter/material.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
+  const DetailScreen({ Key key }) : super(key: key);
+  @override
+  _DetailScreenState createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> with SingleTickerProviderStateMixin {
+  final List<Tab> myTabs = <Tab>[
+    Tab(text: 'LEFT'),
+    Tab(text: 'CENTER'),
+    Tab(text: 'RIGHT')
+  ];
+
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: myTabs.length);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Hero')),
-      body: GestureDetector(
-        onTap: () {
-          Navigator.pop(context);
-        },
-        child: Hero(
-            tag: 'imageHero',
-            child: Image.network('https://raw.githubusercontent.com/flutter/website/master/src/_includes/code/layout/lakes/images/lake.jpg')
+      appBar: AppBar(
+        title: TextField(
+          textInputAction: TextInputAction.search,
+          textAlign: TextAlign.center,
+          decoration: InputDecoration(
+            hintText: '🔍 搜索你喜欢的内容'
+          ),
+        ),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: myTabs,
+          indicatorColor: Colors.red,
+          indicatorSize: TabBarIndicatorSize.label
         ),
       ),
+      body: TabBarView(
+        controller: _tabController,
+        children: myTabs.map((Tab tab) {
+          return Center(child: Text(tab.text));
+        }).toList()
+      )
     );
   }
 }
