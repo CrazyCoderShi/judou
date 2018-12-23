3049de2789d728348a9b22ece5172677146d71c0
天南-mini <crazycoderss@gmail.com>
2018-12-23 22:35:26 +0800

    modify: search textinput

--- lib/discovery/discovery.dart
+++ lib/discovery/discovery.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:judou/utils/color_util.dart';
 
 class DiscoveryPage extends StatefulWidget {
   @override
   _DiscoveryPageState createState() => _DiscoveryPageState();
 }
 
-class _DiscoveryPageState extends State<DiscoveryPage> with SingleTickerProviderStateMixin {
+class _DiscoveryPageState extends State<DiscoveryPage>
+    with SingleTickerProviderStateMixin {
   final List<Tab> myTabs = <Tab>[
     Tab(text: 'LEFT'),
     Tab(text: 'CENTER'),
@@ -30,21 +32,31 @@ class _DiscoveryPageState extends State<DiscoveryPage> with SingleTickerProvider
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: CupertinoTextField(placeholder: '搜索你喜欢的内容', textAlign: TextAlign.center),
-          bottom: TabBar(
-              controller: _tabController,
-              tabs: myTabs,
-              indicatorColor: Colors.black26,
-              indicatorSize: TabBarIndicatorSize.label
+      appBar: AppBar(
+        title: CupertinoTextField(
+          placeholder: '搜索你喜欢的内容',
+          textAlign: TextAlign.center,
+          decoration: BoxDecoration(
+            color: ColorUtils.dividerColor,
+            border: Border.all(color: Colors.transparent),
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(20.0),
           ),
+          style: TextStyle(height: 0.5, fontSize: 16),
         ),
-        body: TabBarView(
-            controller: _tabController,
-            children: myTabs.map((Tab tab) {
-              return Center(child: Text(tab.text));
-            }).toList()
-        )
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: myTabs,
+          indicatorColor: Colors.black26,
+          indicatorSize: TabBarIndicatorSize.label,
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: myTabs.map((Tab tab) {
+          return Center(child: Text(tab.text));
+        }).toList(),
+      ),
     );
   }
 }
