edce6f464d6d2c4fa4577cff32701b4110bc81d1
CrazyCoderShi <crazycoderss@gmail.com>
2018-12-26 14:45:14 +0800

    add: dio

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,8 @@ void main() {
 /// TODO： List
 /// 网络请求: dio -> https://github.com/flutterchina/dio
 class JuDouApp extends StatelessWidget {
+  /// 强制设置splashColor和highlightColor为transparent
+  /// 可以去除material的点击波纹效果
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
