d0a5ca91ee2efbc34e2f51401c7d664c88bfb32a
CrazyCoderShi <crazycoderss@gmail.com>
2019-01-09 18:42:55 +0800

    add: flutter_swiper

--- lib/discovery/widget/recommand_widget.dart
+++ lib/discovery/widget/recommand_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:flutter_page_indicator/flutter_page_indicator.dart';
 
 class RecommandWidget extends StatefulWidget {
   @override
@@ -8,6 +10,28 @@ class RecommandWidget extends StatefulWidget {
 class _RecommandWidgetState extends State<RecommandWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverList(
+          delegate: SliverChildListDelegate([
+            Swiper(
+              itemHeight: 100,
+              itemWidth: MediaQuery.of(context).size.width,
+              itemBuilder: (context, index) {
+                return Container(
+                  height: 100,
+                  width: MediaQuery.of(context).size.width - 40,
+                  color: index == 1 ? Colors.brown : Colors.red,
+                );
+              },
+              itemCount: 3,
+              pagination: SwiperPagination(alignment: Alignment.bottomCenter),
+              indicatorLayout: PageIndicatorLayout.WARM,
+              layout: SwiperLayout.TINDER,
+            ),
+          ]),
+        )
+      ],
+    );
   }
 }
